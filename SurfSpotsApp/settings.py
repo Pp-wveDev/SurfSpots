d3eac4f056c758ed7b7575782444a569dbb1a980
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2020-12-09 17:10:11 +0100

    Added templates folder to config

--- SurfSpotsApp/settings.py
+++ SurfSpotsApp/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'SurfSpotsApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'SurfSpotsWeb', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
